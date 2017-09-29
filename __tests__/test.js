934fa2ba259d37f094b890194c750ecea740f57f
Brandon Dail <brandon.dail@formidable.com>
2017-09-29 10:19:12 -0700

    Clean up test suite

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,28 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactImagePalette from "../lib";
-import image from "./fairlight.png";
-
+import ReactImagePalette, { getImagePalette } from "../lib";
 // Album cover for Com Truise - Fairlight
+import testImage from "./fairlight.png";
 
 class TestComponent extends React.Component {
-  componentDidCatch(err) {
-    console.log(err.message);
-  }
   render() {
     const { render } = this.props;
-    return React.createElement(ReactImagePalette, { image }, render);
+    return React.createElement(ReactImagePalette, { image: testImage }, render);
   }
 }
 
+let container;
+
+const renderWithExpect = (done, palette) => {
+  expect(palette.backgroundColor).to.equal("rgb(60, 16, 32)");
+  expect(palette.color).to.equal("#EF4E2E");
+  expect(palette.alternativeColor).to.equal("#D17872");
+  done();
+  // Return null so React doesn't throw an error
+  // about an empty return
+  return null;
+};
+
 describe("react-image-palette", () => {
-  it("should parse a palette from an image", done => {
-    const container = document.createElement("div");
-    const render = style => {
-      expect(style.backgroundColor).to.equal("rgb(60, 16, 32)");
-      expect(style.color).to.equal("#EF4E2E");
-      expect(style.alternativeColor).to.equal("#D17872");
-      done();
+  beforeEach(() => {
+    container = document.createElement("div");
+  });
+  describe("provider", () => {
+    it("should parse a palette from an image", done => {
+      ReactDOM.render(
+        React.createElement(ReactImagePalette, {
+          image: testImage,
+          render: renderWithExpect.bind(this, done)
+        }),
+        container
+      );
+    });
+    it("should render defaults if the image fails to load", done => {
+      const defaults = {
+        backgroundColor: "red",
+        color: "white",
+        alternativeColor: "blue"
+      };
+      ReactDOM.render(
+        React.createElement(ReactImagePalette, {
+          image: "unknown-image.gif",
+          defaults,
+          render: palette => {
+            expect(palette).to.equal(defaults);
+            done();
+            return null;
+          }
+        }),
+        container
+      );
+    });
+  });
   describe("getImagePallete", () => {
     it("should export getImagePallete for imperative use", done => {
       const image = new Image();
