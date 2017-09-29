795df5c0912b3618b58b9cab26d152a85dfa6334
Brandon Dail <brandon.dail@formidable.com>
2017-09-29 14:25:22 -0700

    Improve demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import ImagePaletteProvider from "react-image-theme-parser";
+import ImagePaletteProvider from "react-image-palette";
+import Spinner from "react-spinkit";
+import "./main.css";
 
 const getAPIURL = artist =>
   `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${encodeURIComponent(
@@ -22,7 +24,10 @@ class App extends Component {
     fetch(getAPIURL(this.state.artist))
       .then(res => res.json())
       .then(json =>
-        this.setState({ albums: json.topalbums.album, loading: false })
+        this.setState({
+          albums: json.topalbums ? json.topalbums.album : [],
+          loading: false
+        })
       );
   };
 
@@ -31,80 +36,76 @@ class App extends Component {
     return image ? image["#text"] : null;
   }
 
+  handleKeyDown = event => {
+    if (event.key === "Enter") {
+      this.updateSearch();
+    }
+  };
+
   render() {
-    const albums = this.state.albums.map(album => {
+    const { albums, loading } = this.state;
+    const renderedAlbums = albums.map(album => {
       const image = this.getLargestImageUrl(album.image);
-      if (image === null) {
+      if (!image) {
         return null;
       }
       return (
-        <ImagePaletteProvider image={image}>
+        <ImagePaletteProvider crossOrigin image={image} key={image}>
           {({ backgroundColor, color, alternativeColor }) => (
-            <div
-              style={{
-                width: 400,
-                margin: 20,
-                padding: 30,
-                backgroundColor,
-                color,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center"
-              }}
-            >
+            <div className="album" style={{ backgroundColor, color }}>
               <h1>{album.name}</h1>
               <a href={album.url} style={{ color: alternativeColor }}>
                 Learn More
               </a>
               <br />
-              <img src={image} style={{ width: 300 }} />
+              <img src={image} />
             </div>
           )}
         </ImagePaletteProvider>
       );
     });
     return (
-      <div style={{ fontFamily: "Nunito", padding: 30, textAlign: "center" }}>
-        <h1 style={{ fontSize: 40, marginBottom: 0 }}>
-          <pre>react-image-palette</pre>
+      <div className="container">
+        <a
+          className="github-ribbon"
+          href="https://github.com/FormidableLabs/react-image-palette/"
+        >
+          <img
+            style={{ position: "absolute", top: 0, right: 0, border: 0 }}
+            src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
+            alt="Fork me on GitHub"
+            data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+          />
+        </a>
+        <h1 className="title">
+          <a href="https://github.com/FormidableLabs/react-image-palette">
+            <pre>react-image-palette</pre>
+          </a>
         </h1>
-        <p style={{ maxWidth: "50%", margin: "auto" }}>
+        <p>
           Search for an artist to see generated color palettes for all the album
           art in their discog.
         </p>
         <br />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginBottom: 20
-          }}
-        >
+        <div className="search--container">
           <input
-            style={{
-              boxSizing: "border-box",
-              height: 40,
-              fontSize: 16,
-              padding: 10,
-              width: "100%",
-              maxWidth: 400
-            }}
+            spellCheck={false}
+            className="search--input"
             value={this.state.artist}
             onChange={this.updateArist}
+            onKeyDown={this.handleKeyDown}
           />
-          <button onClick={this.updateSearch}>Search</button>
+          <button className="search--button" onClick={this.updateSearch}>
+            Search
+          </button>
         </div>
-        <div
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center"
-          }}
-        >
-          {(this.state.loading || !this.state.albums.length) && (
-            <span>Loading...</span>
+        <div className="album--container">
+          {loading && (
+            <div className="spinner--container">
+              <Spinner name="ball-spin-fade-loader" />
+            </div>
           )}
-          {albums}
+          {!loading && renderedAlbums}
         </div>
       </div>
     );
